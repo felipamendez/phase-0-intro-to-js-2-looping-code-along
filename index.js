cf7830f65e252489f34a1210ce24019e36a86dc5
Felipa Mendez <felipalouise@gmail.com>
2021-12-22 14:21:42 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+    function writeCards(friends, events) { 
+        let arr = [];
+        for (let i = 0; i < friends.length; i++) {
+        arr = [...arr,`Thank you, ${friends[i]}, for the wonderful ${events} gift!`];
+        }
+        return arr;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown() {
+    let count = 10; 
+    while (count > -1) {
+    console.log(count--)
+    }
+}
+
+countDown();
